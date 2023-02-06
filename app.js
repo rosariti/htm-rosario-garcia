cb2c2fd6b4667c9480409c4e243844838b0cdf2e
rosariti <123569882+rosariti@users.noreply.github.com>
2023-02-06 12:07:14 -0500

    variables

---
+++ app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMcontentLoaded",() =>{
+const cardsAdj =[
+  name : 'icaro' 
+  img:'images/1 icaro.jpg'
+},name : 'manos' 
+img:'images/2 manos.jpg'}
+,name : 'craneo' 
+img:'images/3 craneo.jpg'}
+,name : 'imagen' 
+img:'images/4 imagen.jpg'}
+,name : 'corazon' 
+img:'images/5 carazon.jpg'}
+,name : 'ojos' 
+img:'images/6 ojos.jpg'}
+,name : 'nube' 
+img:'images/7 nube.jpg'}
+]
+});
