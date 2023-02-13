fd7cb95a5008e9aa15a17f2e56c96bd576488894
rosariti <123569882+rosariti@users.noreply.github.com>
2023-02-13 11:57:23 -0500

    update app.js

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-document.addEventListener("DOMcontentLoaded",() =>{
-const cardsAdj =[
-  name : 'icaro' 
-  img:'images/1 icaro.jpg'
-},name : 'manos' 
-img:'images/2 manos.jpg'}
-,name : 'craneo' 
-img:'images/3 craneo.jpg'}
-,name : 'imagen' 
-img:'images/4 imagen.jpg'}
-,name : 'corazon' 
-img:'images/5 carazon.jpg'}
-,name : 'ojos' 
-img:'images/6 ojos.jpg'}
-,name : 'nube' 
-img:'images/7 nube.jpg'}
-]
+document.addEventListener("DOMcontentLoaded", () => {
+  const cardsAdj = [
+    { name: "icaro", img: "images/1 icaro.jpg" },
+    { name: "manos", img: "images/2 manos.jpg" },
+    { name: "craneo", img: "images/3 craneo.jpg" },
+    { name: "imagen", img: "images/4 imagen.jpg" },
+    { name: "corazon", img: "images/5 carazon.jpg" },
+    { name: "ojos", img: "images/6 ojos.jpg" },
+    { name: "nube", img: "images/7 nube.jpg" },
+    { name: "planeta", img: "images/8 planeta.jpg" }
+  ];
+
+//console.log(cartasrr)
+const cuadricula = document.querySelector(".cudricula");
+const resultado = document.querySelector("#resultado");
+var cartasEscojidas = [];
+var cartasEscojidasId = [];
+var cartasGanadas = [];
+
+function creartablero() {
+  for (let i =0 ;)
+  var carta = document.createElement(`img`);
+  carta.setAttribute("src", "imagenes/reverso.jpg");
+  carta.setAttribute("dta-id", i);
+  carta.addEventListener("click", voltercarta);
+  cuadricula.appendChild(carta);
+}
+
 });
